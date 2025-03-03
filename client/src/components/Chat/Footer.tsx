e3704a1ed61ff876ef488f210a3891be60c9048d
jeonghun-git <wjdgns981211@gmail.com>
2025-03-04 03:20:03 +0900

    third modified

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -37,9 +37,9 @@ export default function Footer({ className }: { className?: string }) {
   const mainContentParts = (
     typeof config?.customFooter === 'string'
       ? config.customFooter
-      : '[LibreChat ' +
+      : '[jeonghun-chat ' +
         Constants.VERSION +
-        '](https://librechat.ai) - ' +
+        '](https://jhun-park.xyz) - ' +
         localize('com_ui_latest_footer')
   ).split('|');
 
